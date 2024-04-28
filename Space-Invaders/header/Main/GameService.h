412ff4da4df9fe4a92e217a20ab564fb451c7f2d
Devisree-Arepalli <73283630+Devisree-Arepalli@users.noreply.github.com>
2024-04-28 23:27:58 +0530

    Update GameService.h

--- Space-Invaders/header/Main/GameService.h
+++ Space-Invaders/header/Main/GameService.h
@@ -1,42 +1,41 @@
 #pragma once
+
 #include <SFML/Graphics.hpp>
 #include "../../header/Global/ServiceLocator.h"
 
-namespace Main
-{
-	enum class GameState
-	{
-		BOOT,
-		SPLASH_SCREEN,
-		MAIN_MENU,
-		INSTRUCTIONS,
-		GAMEPLAY,
-		CREDITS,
-	};
+namespace Main {
 
-	class GameService
-	{
-	private:
-		static GameState current_state;
+    enum class GameState {
+        BOOT,
+        SPLASH_SCREEN,
+        MAIN_MENU,
+        INSTRUCTIONS,
+        GAMEPLAY,
+        CREDITS
+    };
 
-		Global::ServiceLocator* service_locator;
-		sf::RenderWindow* game_window;
+    class GameService {
+    private:
+        static GameState current_state;
+        Global::ServiceLocator service_locator;
+        sf::RenderWindow game_window; // Corrected 
 
-		void initialize();
-		void initializeVariables();
-		void showSplashScreen();
-		void destroy();
+        void initialize();
+        void initializeVariables();
+        void showSplashscreen(); // Corrected function name
+        void destroy();
 
-	public:
-		GameService();
-		virtual ~GameService();
+    public:
+        GameService();
+        virtual ~GameService(); // Corrected syntax
 
-		void ignite();
-		void update();
-		void render();
-		bool isRunning();
+        void ignite(); // Corrected spelling
+        void update();
+        void render();
+        bool isRunning(); // Corrected syntax
 
-		static void setGameState(GameState new_state);
-		static GameState getGameState();
-	};
-}
+        static void setGameState(GameState newState); // Corrected function name and parameter
+        static GameState getGameState();
+    };
+
+} // namespace Main
